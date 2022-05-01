f805608965ac7ae712f2f4c2f7e1bcc0463fad36
tato88 <380685013386@ex.ua>
2022-05-02 01:21:32 +0300

    Structure + NavBar -> Done Preview Pages -> Done Members Page -> Done
    todo: 1.Role not found before any button??
    PUT ROLE IN LOCALSTORAGE

--- src/components/MemberWithRole/MemberWithRole.js
+++ src/components/MemberWithRole/MemberWithRole.js
@@ -4,7 +4,24 @@ import css from "../../pages/RolePage/RolePage.module.css";
 const MemberWithRole = ({member}) => {
 
     const choosenCitizen = (e) => {
-e.preventDefault()
+        e.preventDefault()
+        member.role = 'citizen'
+        console.log(member)
+    }
+    const choosenMafia = (e) => {
+        e.preventDefault()
+        member.role = 'mafia'
+        console.log(member)
+    }
+    const choosenDoctor = (e) => {
+        e.preventDefault()
+        member.role = 'doctor'
+        console.log(member)
+    }
+    const choosenPolice = (e) => {
+        e.preventDefault()
+        member.role = 'police'
+        console.log(member)
     }
 
     return (
@@ -14,9 +31,9 @@ e.preventDefault()
             Choose Your Role:
             <form>
                 <button onClick={choosenCitizen} className={css.citizenBut}>CITIZEN</button>
-                <button className={css.mafiaBut}>MAFIA</button>
-                <button className={css.doctorBut}>DOCTOR</button>
-                <button className={css.policeBut}>POLICE</button>
+                <button onClick={choosenMafia} className={css.mafiaBut}>MAFIA</button>
+                <button onClick={choosenDoctor} className={css.doctorBut}>DOCTOR</button>
+                <button onClick={choosenPolice} className={css.policeBut}>POLICE</button>
             </form>
             <hr/>
         </div>
